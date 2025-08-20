8683750742e1a30ef4ac355019eec38259bfcc82
mani G <gundlamanikumar@gmail.com>
2025-08-20 11:52:30 -0300

    added a new script using argus

---
+++ shell-scripting/04-variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+p1=$1
+p2=$2
+p3=$3
+
+    echo "$p1: Hello, my name is $p1"
+    echo "$p2: Hello my name is $p2, nice to meet you!"
+    echo "$p1: how are you doing you $p2?"
+    echo "$p2: Im good $p1. how are you doing?"
+    echo "$p1: Im doing good $p2. Thank you for asking."
+    echo "$p1: $p2, do you anything this person called $p3"
+    echo "$p2: yeah, I know the person called $p3"
