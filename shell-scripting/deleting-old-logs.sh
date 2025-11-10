921400bde36fb16d842bfc3eedb8740cf6a2c1db
mani G <gundlamanikumar@gmail.com>
2025-11-10 14:15:47 -0400

    deleting files now

--- shell-scripting/deleting-old-logs.sh
+++ shell-scripting/deleting-old-logs.sh
@@ -2,6 +2,11 @@
 
 Source_dir="/tmp/shellscript.log"
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m" 
+
 if [ ! -d $Source_dir ]
 then
     echo -e "$R source directory :- $Source_dir does'nt exist"
