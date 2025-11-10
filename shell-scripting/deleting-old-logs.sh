eeb40cd969bab620651b9bede173008f8ee7e061
mani G <gundlamanikumar@gmail.com>
2025-11-10 11:42:37 -0400

    adding a script

--- shell-scripting/deleting-old-logs.sh
+++ shell-scripting/deleting-old-logs.sh
@@ -2,8 +2,9 @@
 
 Source_dir="/tmp/shell_script.log"
 
-if [ ! -d $Source_dir ] then ;
-echo -e "$R source directory :- $Source_dir does'nt exist"
+if [ ! -d $Source_dir ]
+then
+    echo -e "$R source directory :- $Source_dir does'nt exist"
 fi
 
 file_to_delete=$(find $Source_dir -type f -mtime +21 -name "*.log")
