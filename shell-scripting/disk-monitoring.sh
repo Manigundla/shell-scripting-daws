ffc0005161bcdd4bcab23c356d43384b4ddfdcaa
mani G <gundlamanikumar@gmail.com>
2025-11-12 21:49:55 -0400

    addidng disk monitoring script

--- shell-scripting/disk-monitoring.sh
+++ shell-scripting/disk-monitoring.sh
@@ -6,13 +6,13 @@ message=""
 
 while IFS= read line 
 do 
-    usage=$( echo $line | awk '{print $6F}' | cut -d % -f1)
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
     partition=$(echo $line | awk '{print $1F}')
         if [ $usage -ge $DISK_THRESHOLD ]
         then 
-        message+="High disk usage on $partition : $DISK_USAGE\n"
+            message+="High disk usage on $partition : $DISK_USAGE\n"
         fi
 
 done <<< $DISK_USAGE
 
-echo -e "message:-$message"
+echo -e "message:- $message"
