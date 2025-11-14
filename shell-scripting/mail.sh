ba5e72ece89291cea9f74dcfcb6f7bd8698935e7
mani G <gundlamanikumar@gmail.com>
2025-11-14 10:24:27 -0400

    addidng disk monitoring script

--- shell-scripting/mail.sh
+++ shell-scripting/mail.sh
@@ -7,7 +7,7 @@ TO_ADDRESS=$4
 SUBJECT=$5
 ESCAPED_KEYWORD=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g');
 
-FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" "s/ALERT_TYPE/$ALERT_TYPE/g" "s/BODY/$ESCAPED_BODY/g" templet.html)
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/BODY/$ESCAPED_BODY/g" templet.html)
 #FINAL_BODY=$(sed \
   #-e "s|TO_TEAM|$TO_TEAM|g" \
   #-e "s|ALERT_TYPE|$ALERT_TYPE|g" \
