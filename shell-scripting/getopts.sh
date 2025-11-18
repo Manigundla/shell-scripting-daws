82edcd7becdc3df8075f2d31f674ddcd653e5d92
mani G <gundlamanikumar@gmail.com>
2025-11-18 10:47:28 -0400

    addidng disk monitoring script

--- shell-scripting/getopts.sh
+++ shell-scripting/getopts.sh
@@ -5,20 +5,21 @@ WISHES="How are you doing?"
 
 
 USAGE(){
-    echo "USAGE: $(BASENAME $0) -n<name> -w<wishes>"
+    echo "USAGE: $(basename $0) -n<name> -w<wishes>"
     echo "Options:"
-    echo "-n, specifies the name (mandatory)"
+    echo "-n, specifies the name (mandatory)" 
     echo "-w, specifies the wishes(optional)"
     echo "-h, Displays Help and Exits"
 }
 
-while getopts "-n:-w:-h" opt;
+while getopts "n:w:h" opt;
 do
     case $opt in
-        n) NAME="$OPTARG";;
-        W) WISHES="$OPTARG";;
-        h) USAGE; exit;;
-        :) USAGE; exit;;
+        n) NAME="$OPTARG";; # -n option found, its argument is in $OPTARG
+        W) WISHES="$OPTARG";; # -w option found, its argument is in $OPTARG
+        h) USAGE; exit;; # -h option found (help flag)
+        ?) USAGE; exit 1;; # Catches an invalid option (e.g., -x)
+        :) USAGE; exit;; # Catches missing argument for n or w
     esac
 done
 
